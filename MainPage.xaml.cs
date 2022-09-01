925bf8e69a8a6e26008e8cf7c50d11b1e694a31d
Jim Chapman <home@jim-chapman.net>
2022-09-01 13:31:59 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace VisibilityTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+    }
+
+    private void ShowHideClicked(object sender, EventArgs e)
+    {
+        this.HideableLabel.IsVisible = !this.HideableLabel.IsVisible;
+    }
+}
+
